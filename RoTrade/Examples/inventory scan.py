1a75468a584f0ea447cfaa4bbae533374bcd2e25
walker <100868154+wa1ker38552@users.noreply.github.com>
2022-08-31 09:28:16 -0700

    Update inventory scan.py

--- RoTrade/Examples/inventory scan.py
+++ RoTrade/Examples/inventory scan.py
@@ -6,11 +6,11 @@ params = params.default()
 client = roblox.Client('./token.txt', parameters=params)
 
 def on_ready():
-  print(clinet.user, client.id)
+  print(client.user, client.id)
   
   for item in roblox.Inventory(client.id):
     print(item.id)
   print(roblox.Inventory(client.id).total_values())
 
 if __name__ == '__main__':
-  clinet.run()
+  client.run()
